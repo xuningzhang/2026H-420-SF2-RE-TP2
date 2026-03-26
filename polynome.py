3d6fbc6183618258c5a5c871f22c142c69b0cf91
XuningZhang <xuningzhang1357@gmail.com>
2026-03-26 18:40:22 -0400

    Création de la classe Polynome

--- polynome.py
+++ polynome.py
@@ -5,4 +5,44 @@ class Polynome(Expression):
     """Polynome represente par une liste de coefficients [a0, a1, a2, ...]."""
 
     # Votre code ici (remplacer le "pass" par votre implementation)
-    pass
+    def __init__(self, coeffiscients: list([float | int])):
+        self.coeffiscients = coeffiscients
+    
+    def evaluer(self, x: float) -> float:
+        somme = 0
+        for exp, coeff in enumerate(self.coeffiscients):
+            somme += coeff * (x ** exp)
+        return somme
+    
+    def deriver(self):
+        f_derive = []
+        for exp, coeff in enumerate(self.coeffiscients):
+            f_derive.append(exp*coeff)
+        del f_derive[0]
+        if len(f_derive) == 0:
+            f_derive = [0]
+        return Polynome(f_derive)
+    
+    def __str__(self):
+        expression = ""
+        for exp, coeff in enumerate(self.coeffiscients):
+            if coeff != 0:
+                match exp:
+                    case 0:
+                        exp = ""
+                    case 1:
+                        exp = "x"
+                    case _:
+                        exp = f"x^{exp}"
+                if len(expression) == 0:
+                    expression += f"{coeff}"+exp
+                elif coeff > 0:
+                    expression += f" + {coeff}"+exp
+                else:
+                    expression += f" - {abs(coeff)}"+exp
+        return expression
+
+if __name__ == "__main__":
+    exemple = Polynome([1, 0, -3])
+    print(exemple.deriver())
+    print(exemple)
