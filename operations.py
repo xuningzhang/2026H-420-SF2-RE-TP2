c43dbbf6f1a412c150b87cd91747e646d5dc2f94
XuningZhang <xuningzhang1357@gmail.com>
2026-03-29 18:09:02 -0400

    Création de la classe Multiplication
    Ajout des méthodes et du constructeur dans la classe Multiplication

--- operations.py
+++ operations.py
@@ -22,4 +22,15 @@ class Multiplication(Expression):
     """Expression representant u * v."""
 
     # Votre code ici (remplacer le "pass" par votre implementation)
-    pass
+    def __init__(self, u, v):
+        self.u = u
+        self.v = v
+    
+    def evaluer(self, x):
+        return self.u.evaluer(x)*self.v.evaluer(x)
+    
+    def deriver(self):
+        return Addition(Multiplication(self.u.deriver(), self.v), Multiplication(self.u, self.v.deriver()))
+    
+    def __str__(self):
+        return f"({self.u}) * ({self.v})"
