b9098e9a2a93dc40a4e996f74d1b1ce019166353
XuningZhang <xuningzhang1357@gmail.com>
2026-03-29 17:44:36 -0400

    Création de la classe Addition
    Ajout des méthodes et du constructeur dans la classe Addition

--- operations.py
+++ operations.py
@@ -1,11 +1,21 @@
 from expression import Expression
 
-
 class Addition(Expression):
     """Expression representant u + v."""
 
     # Votre code ici (remplacer le "pass" par votre implementation)
-    pass
+    def __init__(self, u, v):
+        self.u = u
+        self.v = v
+
+    def evaluer(self,x):
+        return sum([function.evaluer(x) for function in [self.u, self.v]])
+    
+    def deriver(self):
+        return Addition(self.u.deriver(), self.v.deriver())
+    
+    def __str__(self):
+        return f"{self.u} + {self.v}"
 
 
 class Multiplication(Expression):
