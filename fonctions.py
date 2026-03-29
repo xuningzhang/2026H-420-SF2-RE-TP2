e4993e5c0dc90aaeac056bed591946cb6489766f
XuningZhang <xuningzhang1357@gmail.com>
2026-03-29 18:27:58 -0400

    Création de la classe Sin
    Ajout des méthodes et du constructeur dans la classe Sin

--- fonctions.py
+++ fonctions.py
@@ -1,12 +1,24 @@
 from expression import Expression
+from polynome import Polynome
+from operations import Multiplication
+import math
 
 
 class Sin(Expression):
     """Expression representant sin(u)."""
 
     # Votre code ici (remplacer le "pass" par votre implementation)
-    pass
+    def __init__(self, function : "Polynome"):
+        self.function = function
+    
+    def evaluer(self, x):
+        return math.sin(self.function.evaluer(x))
 
+    def deriver(self):
+        return Cos(self.function)
+    
+    def __str__(self):
+        return f"sin({self.function})"
 
 class Cos(Expression):
     """Expression representant cos(u)."""
