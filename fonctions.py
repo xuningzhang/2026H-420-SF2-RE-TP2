83043423c7caa59c754bc78ead9891f8f07f7237
XuningZhang <xuningzhang1357@gmail.com>
2026-03-29 20:36:46 -0400

    Création des classes Cos et Exp
    Ajout des méthodes et des constructeurs dans les classes suivantes:
        - Cos (dans fonctions.py)
        - Exp (dans fonctions.py)

--- fonctions.py
+++ fonctions.py
@@ -24,11 +24,39 @@ class Cos(Expression):
     """Expression representant cos(u)."""
 
     # Votre code ici (remplacer le "pass" par votre implementation)
-    pass
+    def __init__(self, function : "Polynome"):
+        self.function = function
+    
+    def evaluer(self, x):
+        return math.cos(self.function.evaluer(x))
+
+    def deriver(self):
+        return Sin(Multiplication(Polynome([-1]),self.function))
+    
+    def __str__(self):
+        return f"cos({self.function})"
 
 
 class Exp(Expression):
     """Expression representant exp(u)."""
 
     # Votre code ici (remplacer le "pass" par votre implementation)
-    pass
+    def __init__(self, function : "Polynome"):
+        self.function = function
+    
+    def evaluer(self, x):
+        return math.e**(self.function.evaluer(x))
+
+    def deriver(self):
+        return Exp(self.function)
+    
+    def __str__(self):
+        return f"e^({self.function})"
+
+if __name__ == "__main__":
+    exemple = [Cos(Polynome([1,2,3])),Sin(Polynome([1,2,3])),Exp(Polynome([1,2,3]))]
+    for func in exemple:
+        print(func)
+        print(func.evaluer(1))
+        print(func.deriver())
+        print(func.deriver().evaluer(1))
